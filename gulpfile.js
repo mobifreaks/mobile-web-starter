06db882f345b9fdac838f168f5fd4e35915cc90e
Munawar <hi@munwr.com>
2017-02-09 14:41:34 +0530

    Adding sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,13 @@ var gulp = require('gulp');
 // gulp plugins
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
   gulp.src('app/scss/style.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(gulp.dest('dist/css'))
+    .pipe(sourcemaps.write())
 });
